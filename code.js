9de92e505d0bdc455f903c770f31da239fab5f4d
Abigail Trapp <abbsthepandahead@gmail.com>
2022-01-31 23:30:24 -0700

    JavaScript edit

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
 
 const Url = 'https://frontend-take-home.fetchrewards.com/form'
-// const Http = new XMLHttpRequest()
+const Http = new XMLHttpRequest()
 
 function getEndpoints(myURL) {
-    // Http.open("GET", myURL)
-    // Http.send()
+    Http.open("GET", myURL)
+    Http.send()
     fetch(myURL)
         .then(response => { return response.json() })
         .then(data => {
@@ -27,33 +27,31 @@ function getEndpoints(myURL) {
 
 getEndpoints(Url)
 
-let btn = document.getElementById("btn")
 
+function submitForm(){
 
-    // Http.open("POST",Url)
-    // Http.send()
-
-let nameInput = document.getElementById("Name").value
-let email = document.getElementById("Email").value
-let password = document.getElementById("Password").value
-let occupationId = document.getElementById("occupationId").value
-let stateId = document.getElementById("stateId").value
+    let btn = document.getElementById("btn")
 
 btn.addEventListener("click", function(event){
     event.preventDefault()
+    
+    let nameInput = document.getElementById("Name").value; 
+    let email = document.getElementById("Email").value ;
+    let password = document.getElementById("Password").value ;
+    let occupationId = document.getElementById("occupationId").value;
+    let stateId = document.getElementById("stateId").value;
+    
 let data = {
-    name : `${nameInput}`,
-    email: `${email}`,
-    password: `${password}`,
-    occupation: `${occupationId}`,
-    state: `${stateId}`
+    "name" : `${nameInput}`,
+    "email": `${email}`,
+    "password": `${password}`,
+    "occupation": `${occupationId}`,
+    "state": `${stateId}`
 }
-
-// console.log(JSON.stringify(data))
 
 fetch(Url,{
     method: "post",
-    headers: {"Content_type": "application/json"},
+    headers: {"Content-type": "application/json"},
     body: JSON.stringify(data)
 })
     .then(response => response.text().then(console.log))
@@ -61,7 +59,7 @@ fetch(Url,{
     .catch(err => console.log(err))
 })
 
-
+}
 
 
 
